c3d4904954ebcc32319c2850dd0efd03b401a739
Klaiv <cmm85@duke.edu>
2014-09-10 18:56:33 -0400

    Empty XCode Project Setup

---
+++ Compassion/Compassion/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Compassion
+//
+//  Created by Clive Mudanda on 9/10/14.
+//  Copyright (c) 2014 Clive Mudanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

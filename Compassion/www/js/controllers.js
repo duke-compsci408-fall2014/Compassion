c3fb7da1c5d04de26ab4b8d713362e4923c1c082
anthony <anthony.olawo@duke.edu>
2014-09-27 14:32:52 -0400

    this one actually works

--- Compassion/www/js/controllers.js
+++ Compassion/www/js/controllers.js
@@ -35,12 +35,10 @@ angular.module('starter.controllers', [])
 
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'Theme 1', id: 1 },
+    { title: 'Theme 2', id: 2 },
+    { title: 'Theme 3', id: 3 },
+
   ];
 })
 

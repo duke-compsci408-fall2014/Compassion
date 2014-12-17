ab48d90fafc8fcb6726506acea1ec9758c166a1e
anthony <anthony.olawo@duke.edu>
2014-12-17 07:13:27 +0300

    pre-merge commit

--- Compassion/www/js/modules/artifact.js
+++ Compassion/www/js/modules/artifact.js
@@ -63,10 +63,8 @@ angular.module('starter')
     $scope.artifactText = $scope.artifacts[$stateParams.artifactId - 1].text;
     $scope.artifactCaption = $scope.artifacts[$stateParams.artifactId - 1].caption;
     $scope.comparativeImages = $scope.artifacts[$stateParams.artifactId - 1].comparativeImages;
-    console.log($scope.comparativeImages); 
     $scope.audioUrl = $sce.trustAsResourceUrl($scope.artifacts[$stateParams.artifactId - 1].audio);
     $scope.playlist = $sce.trustAsResourceUrl($scope.artifacts[$stateParams.artifactId - 1].audio2);
-    //  $scope.video = data[0].artifacts[$stateParams.artifactId -1].video;
     $scope.location = $scope.artifacts[$stateParams.artifactId - 1].location;
   });
 

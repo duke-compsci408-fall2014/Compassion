f133ff67ba61eb763cae59458cb7fd9660e058d9
System Administrator <root@empire04.local>
2015-03-22 08:29:24 -0400

    Nyoirin Kannon high resolution image added

--- Compassion/platforms/android/assets/www/js/modules/artifact.js
+++ Compassion/platforms/android/assets/www/js/modules/artifact.js
@@ -33,9 +33,43 @@ angular.module('starter')
   })
 
 
-.controller('ArtifactCtrl', function($scope, $stateParams, $http, $sce, $cordovaSocialSharing) {
+.controller('ArtifactCtrl', function($scope, $stateParams, $http, $sce, $cordovaSocialSharing, $ionicPopover) {
+  
+  var template = '<ion-popover-view><ion-content> Hello! </ion-content></ion-popover-view>'
+
+  
+
+  // .fromTemplateUrl() method 
+  $ionicPopover.fromTemplateUrl('templates/artifact/social-sharing.html', {
+    scope: $scope,
+  }).then(function(popover){
+    $scope.popover = popover;
+  });
+
+  $scope.openPopover = function($event){
+    $scope.popover.show($event);
+  }
+  $scope.closePopover = function(){
+    $scope.popover.hide(); 
+  }
+
+  //Cleanup popover when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove(); 
+  })
+  //Execute action on hide popover
+  $scope.$on('popover.hidden', function(){
+
+  });
+  //Execute action on remove popover
+  $scope.$on('popover.removed', function(){
+    //Execute action
+  });
+
+
   $scope.curArtId = $stateParams.artifactId;
   $scope.themeId = $stateParams.catId;
+  $scope.returnAddress = ""
 
   $scope.tab = 1;
   $scope.overlay = true;
@@ -44,22 +78,53 @@ angular.module('starter')
   };
 
   $scope.goBack = function() {
-    //window.history.back();
 
-  };
+    if($scope.themeId == "")
+       $scope.returnAddress += "#/app/slideshow"
+      //window.history.back();
+    $scope.returnAddress += "#/app/themes/"
+    $scope.returnAddress += $scope.themeId
+
+    console.log( $scope.returnAddress)
+   };
+
+  $scope.imageURLToShare = function(imageURL){
+    return 'www'+imageURL; 
+  }
 
   $scope.shareAnywhere = function(){ 
     $cordovaSocialSharing.share("This is your message", "This is your subject", "www/img/3.jpg","http://blog.nraboy.com")
   }
 
+
+
   $scope.shareViaTwitter = function(message, image, link) {
-    console.log()
-    $cordovaSocialSharing.canShareVia("twitter", message, image, link).then(function(result){
-      $cordovaSocialSharing.shareViaTwitter(message, image, link);
-    }, function(error) {
-      alert("Cannot share on Twitter");
-    }); 
+    imageURLToShare = 'www/'+image; 
+
+    $cordovaSocialSharing
+      .shareViaTwitter(message, imageURLToShare, link)
+      .then(function(result){
+        //success!
+      } , function(error){
+            alert(error);
+      })
   }
+
+
+
+  $scope.shareViaFacebook = function(message, image, link) {
+    imageURLToShare = 'www/'+image; 
+
+    $cordovaSocialSharing
+      .shareViaFacebook(message, imageURLToShare, link)
+      .then(function(result){
+        //success!
+      } , function(error){
+            alert(error);
+      })
+  }
+
+
 
   $http.get('js/modules/data/data.json').success(function(data) {
     $scope.artifacts = [];
@@ -76,9 +141,14 @@ angular.module('starter')
     $scope.artifactCaption = $scope.artifacts[$stateParams.artifactId - 1].caption;
     $scope.comparativeImages = $scope.artifacts[$stateParams.artifactId - 1].comparativeImages;
     $scope.audioUrl = $sce.trustAsResourceUrl($scope.artifacts[$stateParams.artifactId - 1].audio);
+    $scope.audioUrl2 = $sce.trustAsResourceUrl($scope.artifacts[$stateParams.artifactId - 1].audiob);
     $scope.playlist = $sce.trustAsResourceUrl($scope.artifacts[$stateParams.artifactId - 1].audio2);
+    
     $scope.location = $scope.artifacts[$stateParams.artifactId - 1].location;
   });
+
+
+
 
 })
 
